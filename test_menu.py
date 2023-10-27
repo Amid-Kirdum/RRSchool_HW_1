20724c56a5aeab1e0ad04d9289313e88e7c48ed6
Dmi3 <mudrik1200@gmail.com>
2023-10-28 01:20:29 +0300

    add test_menu and delete test_logout

---
+++ test_menu.py
@@ -0,0 +1,38 @@
+# **Burger menu**
+# 1. Logout
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+#Passed
+def test_auth_valid():
+    driver.get ("https://www.saucedemo.com/")
+
+    url_before = driver.current_url
+
+    username_field = driver.find_element(By.XPATH,'//input[@id="user-name"]')
+    username_field.send_keys('standard_user')
+
+    password_field = driver.find_element(By.XPATH,'//input[@id="password"]')
+    password_field.send_keys('secret_sauce')
+
+    login_button = driver.find_element(By.XPATH,'//input[@id="login-button"]')
+    login_button.click()
+
+    burger_menu = driver.find_element(By.ID, 'react-burger-menu-btn')
+    burger_menu.click()
+    time.sleep(1)
+
+    logout = driver.find_element(By.CSS_SELECTOR,'#logout_sidebar_link')
+    logout.click()
+
+    url_after = driver.current_url
+
+    assert (url_after == url_before)
+
+# 2. Checking the functionality of the "About" button in the menu
+
+# 3. Проверка работоспособности кнопки "Reset App State"
+
