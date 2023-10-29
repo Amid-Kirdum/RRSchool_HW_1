6456716557a6b2341e03013f65893097af375fc5
Dmi3 <mudrik1200@gmail.com>
2023-10-30 00:26:30 +0300

    add test_menu -> button_about

--- test_menu.py
+++ test_menu.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome()
 
 #Passed
 def test_auth_valid():
-    driver.get ("https://www.saucedemo.com/")
+    driver.get("https://www.saucedemo.com/")
 
     url_before = driver.current_url
 
@@ -33,6 +33,35 @@ def test_auth_valid():
     assert (url_after == url_before)
 
 # 2. Checking the functionality of the "About" button in the menu
+
+# FAILED
+def test_button_about():
+    driver.get("https://www.saucedemo.com/")
+
+    username_field = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+    username_field.send_keys('standard_user')
+
+    password_field = driver.find_element(By.XPATH, '//input[@id="password"]')
+    password_field.send_keys('secret_sauce')
+
+    login_button = driver.find_element(By.XPATH, '//input[@id="login-button"]')
+    login_button.click()
+
+    time.sleep(2)
+
+    burger_menu = driver.find_element(By.ID, 'react-burger-menu-btn')
+    burger_menu.click()
+
+    time.sleep(2)
+
+    button_about = driver.find_element(By.XPATH,'//a[@id="inventory_sidebar_link"]')
+    button_about.click()
+
+    time.sleep(2)
+
+    assert driver.current_url == "https://saucelabs.com/"
+
+    driver.quit()
 
 # 3. Проверка работоспособности кнопки "Reset App State"
 
